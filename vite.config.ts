2036ab32f22f7576edaea9812ba2a7139ba2ae50
abhijith-p-subash <abhijith.p.subash@gmail.com>
2025-07-06 22:24:31 +0530

    flow-bite react update

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import flowbiteReact from "flowbite-react/plugin/vite";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(), flowbiteReact()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
